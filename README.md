fdcb4367ab092f08bae3d1d7550b720b6912167a
JJesus Abarca <jesus.abarca.g@gmail.com>
2023-12-24 12:20:28 -0600

    Configuración inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# GQL-PDSS-Surveys-web-client
---
+++ src/main.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+import { sum } from "./main";
+test("subtracts two numbers", () => {
+  expect(sum(10, 7)).toBe(17);
+});
