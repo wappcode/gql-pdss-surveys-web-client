e1030db689d24e77b09c0bcbb34a8547ace2ae0c
JJesus Abarca <jesus.abarca.g@gmail.com>
2023-12-24 13:47:09 -0600

    Se agregaron los tipos, se elimino test de suuma

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,5 @@
-import { test, expect } from "vitest";
-import { sum } from "./main";
-test("subtracts two numbers", () => {
-  expect(sum(10, 7)).toBe(17);
-});
+// import { test, expect } from "vitest";
+// import { sum } from "./main";
+// test("subtracts two numbers", () => {
+//   expect(sum(10, 7)).toBe(17);
+// });
